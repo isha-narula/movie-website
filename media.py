f83bef328e9daa220de4cdcdb1ad89146738ab15
isha-narula <35174509+isha-narula@users.noreply.github.com>
2018-02-23 22:53:14 +0530

    Update media.py

--- media.py
+++ media.py
@@ -2,7 +2,7 @@
 import webbrowser
 
 
-class Movie():
+class Movie(object):
     """This class provides a way to store movie related information.
 Attributes:
         title: A string to store the title of the movie.
@@ -13,13 +13,15 @@ Attributes:
     """
     valid_ratings = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_t, movie_storyl, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         """Initialises Movie class instance variables."""
-        self.title = movie_t
-        self.storyline = movie_storyl
+        self.title = movie_title
+        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
         """Plays the movie trailer in the web browser."""
         webbrowser.open(self.trailer_youtube_url)
+
